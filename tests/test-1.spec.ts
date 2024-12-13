a7f30ef245aa3e57525b2d730bc157a9eec5f33a
Oleksii Plaunov <plaunov@gmail.com>
2024-12-13 19:11:57 +0200

    gitignore + 1st test

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://coffee-cart.app/');
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $0.00');
+  await page.locator('[data-test="Cafe_Breve"]').click();
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $15.00');
+  await page.locator('[data-test="Espresso_Con Panna"]').click();
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $29.00');
+  //something needs to be done here 
+  const checkoutButton = page.locator('[data-test="checkout"]')
+  await checkoutButton.hover();
+  //
+  await page.getByLabel('Remove one Espresso Con Panna').click();
+  await page.getByLabel('Remove one Cafe Breve').click();
+  await expect(page.locator('[data-test="checkout"]')).toContainText('Total: $0.00');
+});
