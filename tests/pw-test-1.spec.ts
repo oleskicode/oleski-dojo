daa52b05ae2f61c51e5cd0dcce3b27a3cd628f0a
Oleksii Plaunov <plaunov@gmail.com>
2024-12-16 14:31:12 +0200

    test1 rewrite continues

--- tests/pw-test-1.spec.ts
+++ tests/pw-test-1.spec.ts
@@ -10,11 +10,16 @@ test('test', async ({ page }) => {
   await expect(page.locator('#introduction + p')).toContainText('Playwright Test was created specifically to accommodate the needs of end-to-end testing.');
 
   //await expect(page.locator('code').filter({ hasText: 'npm init playwright@latest' })).toBeVisible();
-  await expect(page.locator('code').filter({ hasText: 'npm init playwright@latest' })).toBeVisible();
+  await expect(page.locator('#installing-playwright + p + div').filter({ hasText: 'init playwright@latest' })).toBeVisible();
   
   // await page.locator('code').filter({ hasText: 'npm init playwright@latest' }).click();
-  // await page.locator('.tabs > li:nth-child(2)').first().click();
+  await page.locator('#installing-playwright + p + div').filter({hasText: 'init playwright@latest'}).click();
+
+  await page.locator('.tabs > li:nth-child(2)').first().click(); //switch to yarn tab
+
   // await expect(page.locator('code').filter({ hasText: 'yarn create playwright' })).toBeVisible();
+  await expect(page.locator('#installing-playwright + p + div').filter({ hasText: 'yarn create playwright' })).toBeVisible();
+  
   // await page.locator('.tabs > li:nth-child(3)').first().click();
   // await expect(page.locator('code').filter({ hasText: 'pnpm create playwright' })).toBeVisible();
   // await page.locator('.tabs__item').first().click();
