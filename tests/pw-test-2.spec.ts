2b99e305e98c6c63966665df5d98ebb78d5ac87e
Oleksii Plaunov <plaunov@gmail.com>
2024-12-27 18:59:41 +0200

    test 2 upd

--- tests/pw-test-2.spec.ts
+++ tests/pw-test-2.spec.ts
@@ -2,21 +2,26 @@ import { test, expect } from "@playwright/test";
 
 test("Main Page - Main Elements", async ({ page }) => {
   await page.goto("https://playwright.dev/");
-  await expect(page.locator("h1")).toContainText(
+  await expect(page.locator("[class='hero__title heroTitle_ohkl']")).toContainText(
     "Playwright enables reliable end-to-end testing for modern web apps."
   );
-  await expect(
-    page.getByRole("img", { name: "Browsers (Chromium, Firefox," })
-  ).toBeVisible();
-  await page.getByRole("img", { name: "Browsers (Chromium, Firefox," }).click();
-  await expect(
-    page.getByRole("img", { name: "Browsers (Chromium, Firefox," })
-  ).toBeVisible();
-  await page.getByRole("img", { name: "Browsers (Chromium, Firefox," }).click();
-  await expect(page.getByLabel("Search (Command+K)")).toMatchAriaSnapshot(
+  
+  // await expect(
+  //   page.getByRole("img", { name: "Browsers (Chromium, Firefox," })
+  // ).toBeVisible();
+  await expect(page.locator('main div[style="text-align:center"] img')).toBeVisible();
+  
+  // await page.getByRole("img", { name: "Browsers (Chromium, Firefox," }).click();
+  await page.locator('main div[style="text-align:center"] img').click();
+
+  await expect(page.locator('[aria-label="Search (Command+K)"]')).toMatchAriaSnapshot(
     `- button "Search (Command+K)"`
   );
-  await expect(page.getByRole("main")).toContainText(
+
+  // await expect(page.getByRole("main")).toContainText(
+  //   "Any browser • Any platform • One API"
+  // );
+  await expect(page.locator(".row .col h3").first()).toContainText(
     "Any browser • Any platform • One API"
   );
 });
