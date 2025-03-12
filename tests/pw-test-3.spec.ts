69e5ff441eabeea2719a0cf7ca310bf0a2422ea3
Oleksii Plaunov <plaunov@gmail.com>
2025-03-12 18:32:27 +0200

    test3 partially done

--- tests/pw-test-3.spec.ts
+++ tests/pw-test-3.spec.ts
@@ -2,15 +2,27 @@ import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
   await page.goto("https://playwright.dev/");
-  await page.getByLabel("Search (Command+K)").click();
-  await page.getByPlaceholder("Search docs").click();
-  await page.getByPlaceholder("Search docs").fill("ababababa"); //search with no results
-  await expect(page.locator("body")).toContainText(
-    'No results for "ababababa"'
-  );
-  await expect(
-    page.getByRole("button", { name: "Search", exact: true })
-  ).toBeVisible();
+  
+  // await page.getByLabel("Search (Command+K)").click();
+  await page.locator('[aria-label="Search (Command+K)"]').click();
+
+  // await page.getByPlaceholder("Search docs").click();
+  await page.locator(`button[aria-label="Search (Command+K)"]`).click();
+
+  // await page.getByPlaceholder("Search docs").fill("ababababa"); //search with no results
+  await page.locator(`[id="docsearch-input"]`).fill("ababababa"); //search with no results
+
+  // await expect(page.locator("body")).toContainText(
+  //   'No results for "ababababa"'
+  // );
+  await expect(page.locator("body")).toContainText(`No results for "ababababa"`);
+
+  // await expect(
+  //   page.getByRole("button", { name: "Search", exact: true })
+  // ).toBeVisible();
+  await expect(page.locator(`[class="DocSearch-Reset"]`)).toBeVisible();   //[class="DocSearch-Reset"]]
+
+  //TODO finish this
   await expect(page.getByPlaceholder("Search docs")).toHaveValue("ababababa");
   await page.getByLabel("Clear the query").click();
   await expect(page.getByPlaceholder("Search docs")).toBeEmpty();
